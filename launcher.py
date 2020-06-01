71134c9b84644dcef628a8b8b4088826f1d2f600
RaspVor <58893077+RaspVor@users.noreply.github.com>
2020-06-01 22:41:50 +0200

    Create launcher.py

---
+++ launcher.py
@@ -0,0 +1,6 @@
+exec(open("./requirements.py").read())
+exec(open("./classes.py").read())
+exec(open("./functions.py").read())
+exec(open("./Initialisation.py").read())
+exec(open("./GAME.py").read())
+
