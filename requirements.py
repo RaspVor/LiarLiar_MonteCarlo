2414a2389af67ef4eea75c1211121a17ee7e01fc
RaspVor <58893077+RaspVor@users.noreply.github.com>
2020-06-01 22:41:52 +0200

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,7 +1,6 @@
+# Librairies
+
 import random
 import numpy as np
 import numpy.random
 import pandas as pd
-import PySimpleGUI as sg
-import csv
-import gc #https://pysimplegui.readthedocs.io/en/latest/#multiple-threads
